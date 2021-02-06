a731eed2615cf0259c9c3d713d8de8694c8e9338
Johnny Ung <johnnyung91@gmail.com>
2021-02-06 15:33:35 -0800

    enabled sorting by age, type, title

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,13 +1,28 @@
-import React, {useEffect, useState} from 'react';
-import { Box, VStack, Grid, Heading } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import {
+  Flex,
+  Box,
+  VStack,
+  Grid,
+  Heading,
+  Button,
+  Select,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react';
+import { FaChevronDown } from 'react-icons/fa';
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import { SearchForm } from './SearchForm';
 import { useStateValue } from '../StateProvider';
 import { EmployeeTable } from './EmployeeTable';
+import _ from 'lodash';
 
 export const SearchResults = () => {
   const [{ term }, dispatch] = useStateValue();
   const [employees, setEmployees] = useState(null);
+  const [jobTypes, setJobTypes] = useState(null);
 
   useEffect(() => {
     const obj = { term };
@@ -23,10 +38,38 @@ export const SearchResults = () => {
     fetch('http://localhost:5000/api/get-employees', req)
       .then(res => res.json())
       .then(data => {
+        const uniqueJobs = _.uniqBy(data, 'jobType').map(x => x.jobType);
+        //Set Employees
         setEmployees(data);
+        //Set Type List
+        setJobTypes(uniqueJobs);
       });
-
   }, [term]);
+
+  const filterEmployees = filter => {
+    let filtered;
+    switch (filter) {
+      case 'ageAsc':
+        filtered = _.orderBy(employees, ['age'], ['asc']);
+        setEmployees(filtered);
+        break;
+      case 'ageDesc':
+        filtered = _.orderBy(employees, ['age'], ['desc']);
+        setEmployees(filtered);
+        break;
+      case 'jobType':
+        filtered = _.orderBy(employees, ['jobType'], ['asc']);
+        setEmployees(filtered);
+        break;
+      case 'ageDesc':
+        filtered = _.orderBy(employees, ['jobTitle'], ['asc']);
+        setEmployees(filtered);
+        break;
+      default:
+        break;
+    }
+  };
+
 
   return (
     <Box textAlign="center" fontSize="xl">
@@ -35,6 +78,39 @@ export const SearchResults = () => {
         <VStack spacing={8} maxW="1140px" w="100%" m="0 auto" px={2}>
           <Heading>Mocaa Employee Search Page</Heading>
           <SearchForm />
+          <Flex w="100%" justify="space-evenly">
+            <Button>MAGIC BUTTON</Button>
+            <Menu>
+              <MenuButton as={Button} rightIcon={<FaChevronDown />}>
+                Sort By
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={() => filterEmployees('ageAsc')}>
+                  Age Ascending
+                </MenuItem>
+                <MenuItem onClick={() => filterEmployees('ageDesc')}>
+                  Age Descending
+                </MenuItem>
+                <MenuItem onClick={() => filterEmployees('jobType')}>
+                  Job Type
+                </MenuItem>
+                <MenuItem onClick={() => filterEmployees('jobTitle')}>
+                  Job Title
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+          {jobTypes && (
+            <Select w="300px">
+              <option value="">Select</option>
+              {jobTypes.map((x, i) => (
+                <option key={i} value={x}>
+                  {x}
+                </option>
+              ))}
+            </Select>
+          )}
+
           {employees && <EmployeeTable employees={employees} />}
         </VStack>
       </Grid>
